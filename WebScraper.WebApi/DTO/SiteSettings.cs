0e6b270ce4fd56ddb812c5c2f591118261397627
Pugach Alexander <fotone97@yandex.ru>
2020-05-18 00:00:01 +0300

    BaseUrl перенесена в SiteDto

--- WebScraper.WebApi/DTO/SiteSettings.cs
+++ WebScraper.WebApi/DTO/SiteSettings.cs
@@ -7,9 +7,6 @@ namespace WebScraper.WebApi.DTO
     {
         [Key]
         public int Id { get; set; }
-
-        [MaxLength(100)]
-        public string BaseUrl { get; set; } 
         public bool AutoGenerateSchedule { get; set; }
         public TimeSpan MinCheckInterval { get; set; } = TimeSpan.FromMinutes(30);
         public TimeSpan CheckInterval { get; set; } = TimeSpan.FromMinutes(30);
