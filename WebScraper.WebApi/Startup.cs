0a77ae935ecb3f4856e7d7b9c9709d36e475b9e4
Pugach Aleksandr <33941018+PugachA@users.noreply.github.com>
2020-05-24 23:30:44 +0300

    Develop (#20)
    * Add selenuim service (#16)
    
    * Добавлен атрибут для использования Selenium
    
    * Изменена кодировка на Utf-8
    
    * Выделен интерфейс для загрузчиков
    Добавлен SeleniumServixe
    Добавлена фабрика
    
    * Добавлено удаление html пробела
    
    * Косметические изменения
    
    * Добавлено использование фабрики
    Количество браузеров здается в конфиге
    Дополнено DI
    
    * В Staging запускаем 1 драйвер
    
    * Add chrome driver publish (#17)
    
    * Добавлен атрибут для использования Selenium
    
    * Изменена кодировка на Utf-8
    
    * Выделен интерфейс для загрузчиков
    Добавлен SeleniumServixe
    Добавлена фабрика
    
    * Добавлено удаление html пробела
    
    * Косметические изменения
    
    * Добавлено использование фабрики
    Количество браузеров здается в конфиге
    Дополнено DI
    
    * В Staging запускаем 1 драйвер
    
    * Добавлена публикация chome driver
    
    * Добавлена замена запятой на точку
    
    * Добавлено замена html табуляции на пробел
    
    * Add wildberries (#19)
    
    * Добавлен wildberreis
    Расширена дополнительная информация
    
    * Косметические изменения
    
    * Увеличен таймаут для Selenium
    
    * Значение таумаута перенесено в конфиг

--- WebScraper.WebApi/Startup.cs
+++ WebScraper.WebApi/Startup.cs
@@ -64,7 +64,7 @@ namespace WebScraper.WebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SelenuimService selenuimService)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
