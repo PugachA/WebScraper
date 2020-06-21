d2c7d8f67fc17f8ae43875bc637e753f4d44034c
Pugach Alexander <fotone97@yandex.ru>
2020-06-21 23:39:16 +0300

    Начата разработка генератора данных

---
+++ WebScraper.ML.DatasetGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using WebScraper.WebApi.Models;
+using WebScraper.WebApi.Models.Factories;
+
+namespace WebScraper.ML.DatasetGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                 .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                 .AddJsonFile($"parserSettings.json", optional: false, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(configure => configure.AddConsole())
+                //.AddTransient<IConfiguration>(provider => configuration)
+                //.AddTransient<PriceParserFactory>()
+                //.AddTransient<HtmlLoader>()
+                //.AddSingleton<SelenuimService>()
+                //.AddTransient<HtmlLoaderFactory>()
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogInformation("Hello");
+            logger.LogError("dfdfdf");
+
+        }
+    }
+}
