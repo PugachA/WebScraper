f760b27b7f352452eda855f5e55777eb042f13dd
Pugach Alexander <fotone97@yandex.ru>
2020-06-29 23:23:28 +0300

    Добавлена сортировка по нескольким типам

---
+++ WebScraper.ML.DatasetGenerator/LINQExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace WebScraper.ML.DatasetGenerator
+{
+    public static class LINQExtension
+    {
+        public static IEnumerable<T> OfTypes<T>(this IEnumerable<T> collection, Type[] wantedTypes)
+        {
+            if (wantedTypes == null)
+                return null;
+            else
+                return collection.Where(element => wantedTypes.Contains(element.GetType()));
+        }
+    }
+}
