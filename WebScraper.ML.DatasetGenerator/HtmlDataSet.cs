9e5151abd199d8a08d15df7551c5c4477ec8ac35
Pugach Alexander <fotone97@yandex.ru>
2020-06-29 23:23:56 +0300

    Добавлен класс датасета

---
+++ WebScraper.ML.DatasetGenerator/HtmlDataSet.cs
@@ -0,0 +1,13 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.ML.DatasetGenerator
+{
+    public class HtmlDataSet
+    {
+        public bool IsContainsPrice { get; set; }
+        public string HtmlElement { get; set; }
+    }
+}
