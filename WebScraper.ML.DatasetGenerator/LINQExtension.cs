c379396615ea38ff816f0930ec7602bb0f76b268
Pugach Alexander <fotone97@yandex.ru>
2020-07-01 22:27:23 +0300

    Поправлена сортировка по типам

--- WebScraper.ML.DatasetGenerator/LINQExtension.cs
+++ WebScraper.ML.DatasetGenerator/LINQExtension.cs
@@ -8,12 +8,18 @@ namespace WebScraper.ML.DatasetGenerator
 {
     public static class LINQExtension
     {
-        public static IEnumerable<T> OfTypes<T>(this IEnumerable<T> collection, Type[] wantedTypes)
+        public static IEnumerable<T> OfTypes<T>(this IEnumerable<T> source, Type[] wantedTypes)
         {
             if (wantedTypes == null)
-                return null;
-            else
-                return collection.Where(element => wantedTypes.Contains(element.GetType()));
+                throw new ArgumentNullException($"{nameof(wantedTypes)} can not be null");
+
+            if (source == null)
+                throw new ArgumentNullException("Source can not be null");
+
+            foreach (object obj in source)
+                foreach (var type in wantedTypes)
+                    if (type.IsAssignableFrom(obj.GetType()))
+                        yield return (T)obj;
         }
     }
 }
