c9f1d9475e94faad5a9a60315954123ebf65e00e
Pugach Alexander <fotone97@yandex.ru>
2020-07-01 22:27:01 +0300

    Добавлены настройки поиска тега с ценой

---
+++ WebScraper.ML.DatasetGenerator/DatasetGeneratorSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.ML.DatasetGenerator
+{
+    public class DatasetGeneratorSettings
+    {
+        public List<string> PriceTags { get; set; }
+    }
+}
